c7ca0b9025bed730a21a4afc4900f278747df365
thom budel <thombudel@gmail.com>
2017-03-20 16:00:03 +0100

    Add API events show and create

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_and_belongs_to_many :categories
   has_many :photos, dependent: :destroy
   has_many :registrations, dependent: :destroy
@@ -8,8 +8,11 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
   validates :price, presence: true
-  validates :starts_at, presence: true
-  validates :ends_at, presence: true
+
+  #validates :starts_at, presence: true
+  #validates :ends_at, presence: true
+
+
 
   def bargain?
     price < 30
